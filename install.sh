352eee2b87e73c5b0a49e682cdaccc4946efedf5
vmax1g <delfineek2115@gmail.com>
2024-06-23 09:37:56 +0200

    added install script

---
+++ install.sh
@@ -0,0 +1,16 @@
+#/bin/bash
+
+sudo pacman -Syu ttf-meslo-nerd alacritty polybar i3 feh picom rofi stow xorg-xrandr zsh xorg-xinit xorg xorg-server --noconfirm
+cd ~/dotfiles
+stow alacritty
+stow i3
+stow backgrounds
+stow picom
+stow rofi
+stow zsh
+stow neofetch
+stow nvim
+stow polybar
+stow scripts
+
+echo ("installation done! now start xorg with i3")
