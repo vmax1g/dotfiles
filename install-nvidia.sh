679db5b5a2f1512081e20673685f9a5ffd35b979
vmax1g <138099936+vmax1g@users.noreply.github.com>
2024-07-08 16:32:42 +0200

    Update install-nvidia.sh

--- install-nvidia.sh
+++ install-nvidia.sh
@@ -1,1 +1,18 @@
+#/bin/bash
 
+sudo pacman -Syu ttf-meslo-nerd alacritty polybar i3 feh picom rofi stow xorg-xrandr zsh xorg-xinit xorg xorg-server nvidia-dkms linux-headers --noconfirm --needed
+cd ~/dotfiles
+stow alacritty
+stow i3
+stow backgrounds
+stow picom
+stow rofi
+stow zsh
+stow neofetch
+stow nvim
+stow polybar
+stow scripts
+
+echo "exec i3" ~/.zshrc
+
+echo "installation done! now start xorg, or install display manager"
